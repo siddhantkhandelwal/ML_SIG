359672a99636f82cc29f5c77efeee6d1b2840e15
siddhantkhandelwal <siddhant@outlook.com>
2018-06-25 21:30:59 +0530

    Minor fixes in random-wallpaper-wallhaven.py; Second assignment completed. Onto Week 4

--- python_assignment/random-wallpaper-wallhaven.py
+++ python_assignment/random-wallpaper-wallhaven.py
@@ -5,7 +5,7 @@ dir_name = 'wallpapers-wallhaven-toplist'
 if not os.path.exists(dir_name):
     os.makedirs(dir_name)
 print("Downloading Page %s......" % url)
-res = requests.get(url)
+res = requests.get(url, verify=False)
 res.raise_for_status()
 soup = bs4.BeautifulSoup(res.text, "html.parser")
 top_images_list = soup.select('section ul li figure a')
@@ -13,13 +13,13 @@ if top_images_list == []:
     print('Could not find images!')
 else:
     random_image_url = top_images_list[random.randint(1, len(top_images_list))].get('href')
-    res = requests.get(random_image_url)
+    res = requests.get(random_image_url, verify=False)
     res.raise_for_status()
     soup = bs4.BeautifulSoup(res.text, 'html.parser')
     image = soup.select('main section div img')
     image_url = 'https:' + image[0].get('src')
     print('Downloading image.... %s' % image_url)
-    res = requests.get(image_url)
+    res = requests.get(image_url, verify=False)
     res.raise_for_status()
     imageFile = open(os.path.join('wallpapers-wallhaven-toplist', os.path.basename(image_url)), 'wb')
     for chunk in res.iter_content(10000):
